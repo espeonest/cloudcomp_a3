741ec05e61bb7e11b646f565c89b0e1c2c579869
BrittanyRogers <bmrogers@conestogac.on.ca>
2025-11-20 15:23:18 -0500

    Changed backend URLs to render backend URLs

--- frontend/scripts/shoes.js
+++ frontend/scripts/shoes.js
@@ -1,7 +1,7 @@
 const shoesGrid = document.getElementById("shoes-grid");
 
-const API_URL = "http://localhost:8081/shoes";
-const CART_URL = "http://localhost:8081/cart/add/";
+const API_URL = "https://ecommerce-backend-82vp.onrender.com/shoes";
+const CART_URL = "https://ecommerce-backend-82vp.onrender.com/cart/add/";
 
 async function loadShoes() {
   try {
